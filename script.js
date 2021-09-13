cd990ab7446c4617a6ba80939bbb1df740747433
Avneet Kumar Jha <88480400+avneetjha@users.noreply.github.com>
2021-09-13 04:39:15 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let arr = [1, 2, 3, 4, 5];
+
+let new_arr = arr.filter((element) => element % 2 === 0);
+
+// for (var i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     new_arr.push(arr[i]);
+//   }
+// }
+
+console.log(new_arr);
+console.log(arr);
